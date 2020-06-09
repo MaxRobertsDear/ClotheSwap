9355dbe7f19a683ada193fa814bd405c0db552b2
MaxRobertsDear <maxhroberts@gmail.com>
2020-06-09 16:22:01 +0100

    Added Signup logic

---
+++ store/actions/auth.js
@@ -0,0 +1,26 @@
+import { apiKey } from '../../secure-key/keys';
+export const SIGNUP = 'SIGNUP';
+
+export const signup = (email, password) => {
+	return async dispatch => {
+		const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email: email,
+				password: password,
+				returnSecureToken: true,
+			}),
+		});
+
+		if (!response.ok) {
+			throw new Error('Something went wrong!');
+		}
+
+		const resData = await response.json();
+		console.log(resData);
+		dispatch({ type: SIGNUP });
+	};
+};
