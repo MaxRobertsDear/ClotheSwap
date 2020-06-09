0236784139e1628836f121a395878e099b547420
MaxRobertsDear <maxhroberts@gmail.com>
2020-06-09 15:26:45 +0100

    mock auth functionality

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,31 +1,26 @@
-import React from 'react'
-import {
-  ScrollView,
-  Button,
-  Text,
-  View,
-  KeyboardAvoidingView,
-  StyleSheet,
-} from 'react-native'
+import React from 'react';
+import { ScrollView, Button, Text, View, KeyboardAvoidingView, StyleSheet } from 'react-native';
 
-import Colors from '../../constants/Colors'
-import Input from '../../components/UI/Input'
-import { DrawerContentScrollView } from '@react-navigation/drawer'
+import Colors from '../../constants/Colors';
+import Input from '../../components/UI/Input';
+import { DrawerContentScrollView } from '@react-navigation/drawer';
 
-const AuthScreen = (props) => {
-  console.log('AuthScreen props ', props.navigation.setParams)
-  return (
-    <View>
-      <Text>Auth Screen </Text>
-      <Button
-        title='Login'
-        color={Colors.primary}
-        onPress={() => props.setUserIsLoggedIn(true)}
-      />
-    </View>
-  )
-}
+const AuthScreen = props => {
+	console.log('AuthScreen props ', props.navigation.setParams);
+	console.log('AuthScreen props ', props);
+	const clickHandler = () => {
+		props.navigation.setParams({ isLoggedIn: true });
+		props.navigation.navigate('Home');
+	};
 
-const styles = StyleSheet.create({})
+	return (
+		<View>
+			<Text>Auth Screen </Text>
+			<Button title="Login" color={Colors.primary} onPress={clickHandler} />
+		</View>
+	);
+};
 
-export default AuthScreen
+const styles = StyleSheet.create({});
+
+export default AuthScreen;
