16770f7fde92eec10d87bc9a94833dce133283ef
MaxRobertsDear <maxhroberts@gmail.com>
2020-06-11 17:11:50 +0100

    Ran prettier to format all files

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,26 +1,29 @@
-import { apiKey } from '../../secure-key/keys';
-export const SIGNUP = 'SIGNUP';
+import { apiKey } from '../../secure-key/keys'
+export const SIGNUP = 'SIGNUP'
 
 export const signup = (email, password) => {
-	return async dispatch => {
-		const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				email: email,
-				password: password,
-				returnSecureToken: true,
-			}),
-		});
+  return async (dispatch) => {
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+      },
+    )
 
-		if (!response.ok) {
-			throw new Error('Something went wrong!');
-		}
+    if (!response.ok) {
+      throw new Error('Something went wrong!')
+    }
 
-		const resData = await response.json();
-		console.log(resData);
-		dispatch({ type: SIGNUP });
-	};
-};
+    const resData = await response.json()
+    console.log(resData)
+    dispatch({ type: SIGNUP })
+  }
+}
