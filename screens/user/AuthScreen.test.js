e18c87c3545e79c881a0e2e95cb6d020cd047c0a
MaxRobertsDear <maxhroberts@gmail.com>
2020-06-11 17:42:32 +0100

    Added test for AuthScreen - checks that AuthScreen's initial render is as expected - uses reac-test-renderer

--- screens/user/AuthScreen.test.js
+++ screens/user/AuthScreen.test.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import AuthScreen from './AuthScreen';
+import React from 'react'
+import AuthScreen from './AuthScreen'
+import { fireEvent, render, wait } from '@testing-library/react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { rootReducer } from '../../App'
+import renderer from 'react-test-renderer'
 
-import renderer from 'react-test-renderer';
-
-test('renders correctly', () => {
-	const tree = renderer.create(<AuthScreen />).toJSON();
-	expect(tree).toMatchInlineSnapshot();
-});
+function renderWithRedux(
+  ui,
+  { initialState, store = createStore(rootReducer, initialState) } = {},
+) {
+  return {
+    ...renderer.create(<Provider store={store}>{ui}</Provider>).toJSON(),
+    store,
+  }
+}
+// test('renders correctly', async () => {
+// 	const { container, debug, queryByText } = renderWithRedux(<AuthScreen />);
+// 	// debug();
+// 	// expect(queryByText('Password')).toBeTruthy();
+// 	expect(container).toMatchSnapshot();
+// });
+test('renders correctly', async () => {
+  const tree = renderWithRedux(<AuthScreen />)
+  expect(tree).toMatchSnapshot()
+})
