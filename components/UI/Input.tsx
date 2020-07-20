8fe61a49a5e6c0e931b7a738e3303c058d68926a
Max Roberts-Dear <maxroberts-dear@maxs-mbp.home>
2020-07-20 19:33:40 +0100

    Refactored prop types for <Input />

--- components/UI/Input.tsx
+++ components/UI/Input.tsx
@@ -1,12 +1,55 @@
 import React from 'react'
 import { View, Text, TextInput, StyleSheet } from 'react-native'
 
-const Input = (props: any) => {
+interface iInput {
+  label: string;
+  errorText: boolean;
+  placeholder?: string;
+  keyboardType?:
+    | 'default'
+    | 'email-address'
+    | 'numeric'
+    | 'phone-pad'
+    | 'number-pad'
+    | 'decimal-pad'
+    | 'visible-password'
+    | 'ascii-capable'
+    | 'numbers-and-punctuation'
+    | 'url'
+    | 'name-phone-pad'
+    | 'twitter'
+    | 'web-search'
+    | undefined;
+  autoCapitalize?: 'none' | 'sentences' | 'words' | 'characters' | undefined;
+  secureTextEntry?: boolean;
+  minLength?: number;
+  returnKeyType?:
+    | 'default'
+    | 'none'
+    | 'done'
+    | 'go'
+    | 'next'
+    | 'search'
+    | 'send'
+    | 'previous'
+    | 'google'
+    | 'join'
+    | 'route'
+    | 'yahoo'
+    | 'emergency-call'
+    | undefined;
+  value?: string;
+  onChangeText?: (input: string) => void;
+  autoCorrect?: boolean;
+  multiline?: boolean;
+}
+
+const Input = ({ label, errorText, ...props }: iInput) => {
   return (
     <View style={styles.formControl}>
-      <Text style={styles.label}>{props.label}</Text>
+      <Text style={styles.label}>{label}</Text>
       <TextInput {...props} style={styles.input} />
-      {!props.errorText && <Text>Please enter a valid {props.label}</Text>}
+      {!errorText && <Text>Please enter a valid {label}</Text>}
     </View>
   )
 }
